92ae555959794d15266b540968d66b90f36a047e
jalmontegit <jalmonte721@gmail.com>
2026-01-14 20:54:09 -0500

    Django URLs and Views setup

---
+++ main_app/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def home(request):
+    # Send a simple HTML response
+    return HttpResponse('<h1>Hello Team 🧠</h1>')
