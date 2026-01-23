217b78ea74f5a4eca8f06919b92bc98179b3d71c
evanmarshall-dev <me@evanmarshall.dev>
2026-01-22 20:09:06 -0400

    fix(cloudinary): use ImageField instead of FileField for profile images

--- main_app/models.py
+++ main_app/models.py
@@ -15,7 +15,7 @@ class Profile(models.Model):
     program_type = models.CharField(max_length=100, null=True, blank=True)
     grad_year = models.IntegerField(null=True, blank=True)
     portfolio_url = models.URLField(null=True, blank=True)
-    image = models.FileField(upload_to="profile_images/", blank=True)
+    image = models.ImageField(upload_to="profile_images/", blank=True)
 
     def __str__(self):
         return f"Profile of {self.user.username}"
